8794dd8daddaab45ef6b5ab137eaf7480869a1b0
arjun <arjunashok7070>
2023-10-16 08:58:19 +0530

    python

--- ran.py
+++ ran.py
@@ -39,69 +39,65 @@
 #     print("enter valid choice")
 
 
-# import random
-
-# # List of fruits
-# fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
-
-# # Randomly select a fruit from the list
-# selected_fruit = random.choice(fruits)
-
-# # Initialize variables
-# max_chances = 5
-# chances_left = max_chances
-# guessed_word = ["_"] * len(selected_fruit)
-
-# print("Welcome to the Fruit Guessing Game!")
-# print("You have 5 chances to guess the fruit letter by letter.")
-
-# while chances_left > 0:
-#     # Display the current state of the guessed word
-#     print(" ".join(guessed_word))
-
-#     # Get a letter guess from the user
-#     letter_guess = input("Guess a letter: ").lower()
-
-#     if len(letter_guess) != 1 or not letter_guess.isalpha():
-#         print("Please enter a single letter.")
-#         continue
-
-#     if letter_guess in selected_fruit:
-#         for i in range(len(selected_fruit)):
-#             if selected_fruit[i] == letter_guess:
-#                 guessed_word[i] = letter_guess
-#         if "".join(guessed_word) == selected_fruit:
-#             print("Congratulations! You guessed the fruit: " + selected_fruit)
-#             break
-#     else:
-#         chances_left -= 1
-#         print(f"Wrong guess. {chances_left} {'chance' if chances_left == 1 else 'chances'} left.")
-
-# if chances_left == 0:
-#     print(f"Sorry, you're out of chances. The fruit was '{selected_fruit}'.")
-
 import random
-list=['apple','apple','apple']
+list=['apple','banana','grape']
 f=random.choice(list)   
-chance=5
-chances=chance
+totalchance=5
+chances=totalchance
 word=["_"] * len(f)
 while chances > 0:
     print(" ".join(word))
     n=input("guess the fruit:")
-    if len(n)!=1 or not n.isalpha():
+    if len(n)!=1:
         print("enter single letter")
         continue
     if n in f:
         for i in range(len(f)):
             if f[i]==n:
-                word[i]==n
+                word[i]=n
         if "".join(word)==f:
-            print("correct")
+            print("correct",f)
             break
     else:
-        chances=chances-1
+        chances -= 1
         print("chance left",chances)
 
 if chances==0:
-    print("out of chances")
+    print("out of chances",f)
+
+# pas='1234'
+# f=["*"]*len(pas)
+# tc=3
+# c=tc
+# while c>0:
+#     print(" ".join(f))
+#     n=input("enter password")
+#     if n==pas:
+#         print("login successful")
+#         break
+#     else:
+#         c-=1
+#         print("error wrong password")
+# if c==0:
+#     print("try again")
+
+# user=['ar','ab']
+# pas=['12','jjj']
+# tc=3
+# c=tc
+# while c>0:
+#     u=input("enter username")
+#     for i in range(len(user)):
+#         if u==user[i]:
+#             p=input("password:")
+#             if p==pas[i]:
+#                 print("login succesful")
+#                 break
+#             else:
+#                 c-=1
+#                 print("error wrong password")
+# if c==0:
+#     print("try again")
+
+            
+

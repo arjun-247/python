aa33a18fd54edefe329d9dde45f10e52a6d59511
rishab <rishabubaid12@gmail.com>
2023-10-20 08:09:14 +0530

    python

--- ran.py
+++ ran.py
@@ -39,31 +39,31 @@
 #     print("enter valid choice")
 
 
-import random
-list=['apple','banana','grape']
-f=random.choice(list)   
-totalchance=5
-chances=totalchance
-word=["_"] * len(f)
-while chances > 0:
-    print(" ".join(word))
-    n=input("guess the fruit:")
-    if len(n)!=1:
-        print("enter single letter")
-        continue
-    if n in f:
-        for i in range(len(f)):
-            if f[i]==n:
-                word[i]=n
-        if "".join(word)==f:
-            print("correct",f)
-            break
-    else:
-        chances -= 1
-        print("chance left",chances)
+# import random
+# list=['apple','banana','grape']
+# f=random.choice(list)   
+# totalchance=5
+# chances=totalchance
+# word=["_"] * len(f)
+# while chances > 0:
+#     print(" ".join(word))
+#     n=input("guess the fruit:")
+#     if len(n)!=1:
+#         print("enter single letter")
+#         continue
+#     if n in f:
+#         for i in range(len(f)):
+#             if f[i]==n:
+#                 word[i]=n
+#         if "".join(word)==f:
+#             print("correct",f)
+#             break
+#     else:
+#         chances -= 1
+#         print("chance left",chances)
 
-if chances==0:
-    print("out of chances",f)
+# if chances==0:
+#     print("out of chances",f)
 
 # pas='1234'
 # f=["*"]*len(pas)
@@ -81,23 +81,37 @@ if chances==0:
 # if c==0:
 #     print("try again")
 
-# user=['ar','ab']
-# pas=['12','jjj']
+# user='user'
+# pas='password'
 # tc=3
 # c=tc
 # while c>0:
 #     u=input("enter username")
-#     for i in range(len(user)):
-#         if u==user[i]:
-#             p=input("password:")
-#             if p==pas[i]:
-#                 print("login succesful")
-#                 break
-#             else:
-#                 c-=1
-#                 print("error wrong password")
+#     p=input("password:")
+#     if u==user and p==pas :
+#         print("login succesful")
+#         break
+#     else:
+#         c-=1
+#         print("enter correct username & password")
 # if c==0:
 #     print("try again")
 
-            
+# def pas(pwd):
+#     val=1
+#     if len(pwd)<8:
+#         val=0
+#     if not any(char.isupper() for char in pwd):
+#         val=0
+#     if not any(char.isdigit() for char in pwd):
+#         val=0
+#     if val==1:
+#         return val
+
+# pwd=input("enter password")
+# if (pas(pwd)):
+#     print("valid")
+# else:
+#     print("invalid")
+
 

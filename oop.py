1e26d65581fd4f42c28822af4338881e5d6f94a1
rishab <rishabubaid12@gmail.com>
2023-10-31 08:59:17 +0530

    oop

--- oop.py
+++ oop.py
@@ -14,7 +14,7 @@
 # print(r)
 # r.getArea()
 # r.getCircumference()
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 
 # class temperature:
 #     def getDegree(self,fahren):
@@ -32,3 +32,118 @@
 # print(f"temperature in degree celsius {result1}C")
 
 
+# class student:
+#     def __init__(self,name,rno) :
+#         self.name=name
+#         self.rollno=rno
+#     def display(self):
+#         print(self.name,self.rollno)
+#     def setage(self,age):
+#         self.age=age
+#     def setmark(self,mark):
+#         self.marks=mark
+#     def dis(self):
+#         print(self.age,self.marks)
+# s=student("john",18)
+# s.display()
+# s.setage(24)
+# s.setmark(100)
+# s.dis()
+
+
+# class Time():
+#     def __init__(self,hours,mins) :
+#         self.hours=hours
+#         self.mins=mins
+#     def addtime(t1,t2):
+#         t3=Time(0,0)
+#         if t1.mins + t2.mins > 60:
+#             t3.hours=(t1.mins+t2.mins)/60
+#             t3.hours=t3.hours+t1.hours+t2.hours
+#             t3.mins=(t1.mins+t2.mins)-(((t1.mins+t2.mins)/60)*60)
+#             return t3
+#     def displayTime(self):
+#         print("time is",self.hours,"hours and",self.mins,"minutes")
+#     def minutes(self):
+#         print(int(self.hours*60)+self.mins)
+# a=Time(2,50)
+# b=Time(1,20)
+# c=Time.addtime(a,b)
+# c.displayTime()
+# c.minutes()
+
+
+# class bank:
+#     def __init__(self,name,acc,bal) :
+#         self.name=name
+#         self.accountnumber=acc
+#         self.balance=bal
+#     def deposit(self,amount):
+#         self.balance=self.balance+amount
+#         print("balance is",self.balance)
+#     def withdrawal(self,wit):
+#         self.balance-=wit
+#         print("balance is",self.balance)
+# n=input("enter name")
+# a=input("enter account number")
+# b=int(input("enter balance"))
+# am=int(input("enter amount to deposit"))
+# d=int(input("enter amount to withdraw"))
+# bnk=bank(n,a,b)
+# bnk.deposit(am)
+# bnk.withdrawal(d)
+
+
+
+# class bank:
+#     def __init__(self,name,acc,bal) :
+#         self.name=name
+#         self.accountnumber=acc
+#         self.balance=bal
+#     def deposit(self):
+#         amount=int(input("enter amount to deposit"))
+#         self.balance=self.balance+amount
+        
+#     def withdrawal(self):
+#         wit=int(input("enter amount to withdraw"))
+#         self.balance-=wit
+        
+#     def showbal(self):
+#         print(self.name,'\n',self.balance)
+# l=[]
+# def create():
+#     n=input("enter name")
+#     a=int(input("enter account number"))
+#     b=int(input("enter balance"))
+#     bnk=bank(n,a,b)
+#     l.append(bnk)
+# while True:
+#     choice=int(input("enter choice 1.create 2.deposit 3.withdraw 4.exit:"))
+#     if choice==1:
+#         create()
+#     elif choice==2:
+#         accno=int(input("enter account number"))
+#         for i in l:
+#             if accno==i.accountnumber:
+#                 i.deposit()
+#                 i.showbal()
+#     elif choice==3:
+#         accno=int(input("enter account number"))
+#         for i in l:
+#             if accno==i.accountnumber:
+#                 i.withdrawal()
+#                 i.showbal()
+#     elif choice==4:
+#         break
+#     else:
+#         print("invalid")
+#         break
+    
+
+class Employee:
+    def __init__(self,id,name,salary,dep) :
+        self.emp_name=name
+        self.emp_id=id
+        self.emp_salary=salary
+        self.emp_dept=dep
+    
